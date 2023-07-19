26784bf22e2af5461bc10a7eaa4ab9bdd2bff519
Lawrence <lawrencechan27@gmail.com>
2023-07-19 22:48:15 +1000

    Added switch between X and O turns. Added win conditions

--- noughts-and-crosses.js
+++ noughts-and-crosses.js
@@ -3,8 +3,34 @@ const gameboard = (() => {
 
     let tiles = ["", "", "", "", "", "", "", "", ""];
 
-    const setTile = (tile, value) => {
-        tiles[tile] = value;
+    let currentTurn = "X";
+
+    const setTile = (tile) => {
+        if (currentTurn === "X") {
+            tiles[tile] = "X";
+            currentTurn = "O";
+        } else {
+            tiles[tile] = "O";
+            currentTurn = "X";
+        }
+        checkWin();
+    }
+
+    const checkWin = () => {
+        function checkEquality(...values) {
+            values.sort();
+            if (values[0] === values[values.length - 1] && values[0] != "") {
+                alert('game over');
+            }
+        }
+        checkEquality(tiles[0], tiles[1], tiles[2]);
+        checkEquality(tiles[3], tiles[4], tiles[5]);
+        checkEquality(tiles[6], tiles[7], tiles[8]);
+        checkEquality(tiles[0], tiles[3], tiles[6]);
+        checkEquality(tiles[1], tiles[4], tiles[7]);
+        checkEquality(tiles[2], tiles[5], tiles[8]);
+        checkEquality(tiles[0], tiles[4], tiles[8]);
+        checkEquality(tiles[2], tiles[4], tiles[6]);
     }
 
     return { tiles, setTile };
@@ -32,16 +58,15 @@ const dom = (() => {
         let tiles = document.querySelectorAll('.tile');
         tiles.forEach(function (tile) {
             let tileNumber = tile.getAttribute('data-tile-number');
-            tile.addEventListener('click', function (event) { console.log(event); gameboard.setTile(tileNumber, "O"); renderGameboard(gameboard.tiles); });
+            // only apply eventListener if tile hasn't already been clicked
+            if (gameboard.tiles[tileNumber] === "") {
+                tile.addEventListener('click', (event) => { gameboard.setTile(tileNumber); renderGameboard(gameboard.tiles); });
+            }
         });
     }
 
     return { renderGameboard };
 })();
-
-// test stuff
-gameboard.setTile(0, "O");
-gameboard.setTile(2, "X");
 
 // render the gameboard
 dom.renderGameboard(gameboard.tiles);
